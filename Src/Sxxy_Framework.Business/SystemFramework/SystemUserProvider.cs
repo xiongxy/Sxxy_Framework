c53b13e29e45bb0db46cdbc4a4d8d89d2e24eec3
xiongxy <xxyWindows@hotmail.com>
2017-05-22 19:20:45 +0800

    //修改项目位置

--- Src/Sxxy_Framework.Business/SystemFramework/SystemUserProvider.cs
+++ Src/Sxxy_Framework.Business/SystemFramework/SystemUserProvider.cs
@@ -3,11 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Linq.Expressions;
 using System.Text;
-using Sxxy_Framework.DataAccess;
-using Sxxy_Framework.Model.SystemFrameworkModel;
-using Sxxy_Framework.Repository;
-using Sxxy_Framework.Repository.EntityRepository;
-using Sxxy_Framework.Repository.IEntityRepository;
+
 
 namespace Sxxy_Framework.Business.SystemFramework
 {
